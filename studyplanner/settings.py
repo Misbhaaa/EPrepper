a8398b4bfec88ddbaccfdb51b50e0fdd1add3724
Nafeesath Misbha C B <151890278+Misbhaaa@users.noreply.github.com>
2026-03-02 18:21:17 +0530

    Update settings.py

--- studyplanner/settings.py
+++ studyplanner/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'misbhanafeesath@gmail.com'
-EMAIL_HOST_PASSWORD = 'sgwz hizt jids hwqj'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
